fec824bfdad2e1f98a2e9fa0d7d97e226918c101
ThottaTharani <94038108+ThottaTharani@users.noreply.github.com>
2022-04-03 16:04:07 +0530

    Add files via upload

---
+++ Miniproject_C/3_Implementation/inc/header1.h
@@ -0,0 +1,3 @@
+int readMatrix(int arr[50][50], int rows, int colums);
+int printMatrix(int arr[50][50], int rows, int colums);
+int matrixAddSub(int arr1[50][50], int arr2[50][50], int rows, int colums, int mul);
---
+++ Miniproject_C/3_Implementation/inc/header2.h
@@ -0,0 +1,3 @@
+int matrixScalarMultiply(int arr[50][50], int scalar, int rows, int colums);
+int matrixMultiply(int arr1[50][50], int arr2[50][50], int rowsA, int columsA,int columsB);
+int matrixTranspose(int arr1[50][50], int arr2[50][50], int rowsA, int columsA);
